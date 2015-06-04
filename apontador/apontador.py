6e69b7320b8029be78c47ae4a31ebc30e9a383d8
satyandra babu <satyandrab@gmail.com>
2015-06-04 13:24:08 +0530

    updated script

--- apontador/apontador.py
+++ apontador/apontador.py
@@ -2,16 +2,46 @@
 
 
 from urllib2 import Request, urlopen, URLError
-import re, math, csv
-from atom import Title
+import re, math, csv, mechanize, cookielib
+
+
+# Browser
+br = mechanize.Browser()
+
+# Cookie Jar
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
+
+# Browser options
+br.set_handle_equiv(True)
+br.set_handle_gzip(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+
+# Follows refresh 0 but not hangs on refresh > 0
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+# Want debugging messages?
+#br.set_debug_http(True)
+#br.set_debug_redirects(True)
+#br.set_debug_responses(True)
+
+# User-Agent (this is cheating, ok?)
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 def extract_information(url, number_of_pages, data_writer):
     for page in range(1,number_of_pages+1):
         pagination_url = url+'&page='+str(page)
         print pagination_url
-        req = Request(pagination_url)
-        response = urlopen(req)
-        pagination_html = response.read().replace('\r', '').replace('\n', '')
+        
+        html_response = br.open(pagination_url)
+        html_source = html_response.read()
+        pagination_html = html_source.replace('\n', '').replace('\r', '')
+    
+        #req = Request(pagination_url)
+        #response = urlopen(req)
+        #pagination_html = response.read().replace('\r', '').replace('\n', '')
         
         get_details_page_url = re.findall(r'<h3 class="poi name">.*?<a href="(.*?)"', pagination_html)
         for details_url in get_details_page_url:
@@ -19,48 +49,57 @@ def extract_information(url, number_of_pages, data_writer):
         
 def extract_details(details_url, file_name):
     print details_url
-    req = Request(details_url)
-    response = urlopen(req)
-    details_html = response.read().replace('\r', '').replace('\n', '')
+    
+    html_response = br.open(details_url)
+    html_source = html_response.read()
+    details_html = html_source.replace('\n', '').replace('\r', '')
+    
+    #req = Request(details_url)
+    #response = urlopen(req)
+    #details_html = response.read().replace('\r', '').replace('\n', '')
     
     try:
-        phone_number = re.findall(r'<strong itemprop="telephone".*?data-content="\((.*?)"', details_html)[0].replace(')','')
+        phone_number = re.sub('&.*?;', '', re.findall(r'<strong itemprop="telephone".*?data-content="\((.*?)"', details_html)[0].replace(')',''))
     except:
         phone_number = ''
     #print phone_number
     
     try:
-        title = re.findall(r'<h1 class="title" itemprop="name".*?>(.*?)</h1>', details_html)[0].strip().replace('&amp;', '&')
+        title = re.sub('&.*?;', '', re.findall(r'<h1 class="title" itemprop="name".*?>(.*?)</h1>', details_html)[0].strip().replace('&amp;', '&'))
     except:
         title = ''
     #print title
     
     try:
-        street_address = re.sub('\s+', ' ', re.sub('<.*?>','', re.findall(r'<span itemprop="streetAddress">.*?<br>', details_html)[0]).replace('  ','')).strip().replace(',',';').replace('&amp;', '&')
+        street_address = re.sub('&.*?;', '', re.sub('\s+', ' ', re.sub('<.*?>','', re.findall(r'<span itemprop="streetAddress">.*?<br>',
+                                                                          details_html)[0]).replace('  ','')).strip().replace(',','-').replace('&amp;', '&'))
     except:
         street_address = ''
-    print street_address
+    #print street_address
     
     try:
-        address_locality = re.findall(r'<meta property="business:contact_data:locality" content="(.*?)" />', details_html)[0].strip().replace('&amp;', '&')
+        address_locality = re.sub('&.*?;', '', re.findall(r'<meta property="business:contact_data:locality" content="(.*?)" />',
+                                                          details_html)[0].strip().replace('&amp;', '&'))
     except:
         address_locality = ''
     #print address_locality
     
     try:
-        address_region = re.findall(r'<meta property="business:contact_data:region" content="(.*?)" />', details_html)[0].strip().replace('&amp;', '&')
+        address_region = re.sub('&.*?;', '', re.findall(r'<meta property="business:contact_data:region" content="(.*?)" />',
+                                                        details_html)[0].strip().replace('&amp;', '&'))
     except:
         address_region = ''
     #print address_region
     
     try:
-        address_country = re.findall(r'<meta property="business:contact_data:country_name" content="(.*?)" />', details_html)[0].strip().replace('&amp;', '&')
+        address_country = re.sub('&.*?;', '', re.findall(r'<meta property="business:contact_data:country_name" content="(.*?)" />',
+                                                         details_html)[0].strip().replace('&amp;', '&'))
     except:
         address_country
     #print address_country
     
     try:
-        postal_code = re.findall(r'itemprop="postalCode">(.*?)</a>', details_html)[0].strip().replace('&amp;', '&')
+        postal_code = re.sub('&.*?;', '', re.findall(r'itemprop="postalCode">(.*?)</a>', details_html)[0].strip().replace('&amp;', '&'))
     except:
         postal_code = ''
     #print postal_code
@@ -93,7 +132,6 @@ def validate_url(url, data_writer):
             print "Not able to get pages....."
         
 if __name__ == "__main__":
-    #extract_details('http://www.apontador.com.br/local/sp/sao_paulo/igrejas/9682Y5K6/igreja_sao_jose_operario.html')
     
     #http://www.apontador.com.br/local/search.html?q=S%C3%A3o+Jos%C3%A9+do+Rio+Preto&loc_z=S%C3%A3o+Paulo&loc=S%C3%A3o+Paulo%2C+SP&loc_y=S%C3%A3o+Paulo%2C+SP
     file_name = raw_input('Enter name of file to save data(need not to enter file extension)......\n')
@@ -105,4 +143,3 @@ if __name__ == "__main__":
     data_writer.writerow(['URL', 'Title', 'Phone', 'Street Address', 'Locality', 'Region', 'Country', 'Postal Code'])
     url = raw_input('Enter search URL to extract information......\n')
     validate_url(url, data_writer)
-    #extract_details('http://www.apontador.com.br/local/sp/cosmorama/alimentos_e_bebidas/8844Y7YB/baraldi_baraldi_cabral.html', data_writer)
